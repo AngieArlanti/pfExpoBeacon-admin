4878afcf136a62c607e05a64b8c80ea089ebcb68
AngieArlanti <angeles.arlanti@mercadolibre.com>
2020-03-07 15:37:49 -0300

    add argon-dashboard-react template

--- frontend-pf-expo-beacon-admin/src/App.js
+++ frontend-pf-expo-beacon-admin/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import AdminApp from './component/AdminApp';
 
+import "./assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/scss/argon-dashboard-react.scss";
+
 class App extends Component {
   render() {
     return (
--- frontend-pf-expo-beacon-admin/src/component/ListStandsComponent.jsx
+++ frontend-pf-expo-beacon-admin/src/component/ListStandsComponent.jsx
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
 import AdminDataService from '../service/AdminDataService'
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
 
 class ListStandsComponent extends Component {
 
@@ -51,40 +66,62 @@ class ListStandsComponent extends Component {
     render() {
         return (
             <div className="container">
-                <h3>All Stands</h3>
-                {this.state.message && <div class="alert alert-success">{this.state.message}</div>}
-                <div className="container">
-                    {/* <div className="float-right">
+                <Card className="shadow">
+                <CardHeader className="border-0">
+                  <Row className="align-items-center">
+                    <div className="col">
+                      <h3 className="mb-0">All Stands</h3>
+                    </div>
+                    {this.state.message && <div class="alert alert-success">{this.state.message}</div>}
+                    <div className="col text-right">
+                      {/* <Button
+                        color="primary"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="sm"
+                      >
+                        Add Stand
+                      </Button> */}
+                      {/* TODO (ma 2020-03-07) Pasar este button div class a formato Button */}
+                      {/* <div className="float-right">
                          <button className="btn btn-success" onClick={this.addStandClicked}>Add Stand</button>
                      </div> */}
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.stands.map(
-                                    stand =>
-                                        <tr key={stand.id}>
-                                            <th>{stand.cover}</th>
-                                            <td>{stand.title}</td>
-                                            <td>{stand.short_description}</td>
-                                            <td>
-                                                {/* <button className="btn btn-outline-secondary" onClick={() => this.editStandClicked(stand.id)}>Edit</button> */}
-                                                <button className="btn btn-outline-danger" onClick={() => this.deleteStandClicked(stand.id)}>Delete</button>
-                                            </td>
-                                        </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
-                </div>
+                    </div>
+                  </Row>
+                </CardHeader>
+                <Table className="align-items-center table-flush" responsive>
+                  <thead className="thead-light">
+                    <tr>
+                      <th scope="col"></th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                        {
+                            this.state.stands.map(
+                                stand =>
+                                    <tr key={stand.id}>
+                                      {/* TODO (ma 2020-03-07) Ajustar el tamaño de la imagen */}
+                                        <th><img 
+                                        src={stand.cover} 
+                                        height="200px" width="250px"
+                                        /></th>
+                                        <td>{stand.title}</td>
+                                        <td>{stand.short_description}</td>
+                                        <td>
+                                            {/* <button className="btn btn-outline-secondary" onClick={() => this.editStandClicked(stand.id)}>Edit</button> */}
+                                            <button className="btn btn-outline-danger" onClick={() => this.deleteStandClicked(stand.id)}>Delete</button>
+                                        </td>
+                                    </tr>
+                            )
+                        }
+                    </tbody>
+                </Table>
+              </Card>
             </div>
+
         )
     }
 }
