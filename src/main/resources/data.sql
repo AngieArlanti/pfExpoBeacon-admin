7b7f3e173e63cbde3def46da72f37edf26cdd404
AngieArlanti <angeles.arlanti@mercadolibre.com>
2020-03-06 21:36:38 -0300

    Data adjustment

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:08:FC:DD', false, -34.6403175, -58.4018125);
-INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:04:19:2F', false, -33.6403175, -59.4018125);
-INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:04:18:A0', false, -32.6403175, -50.4018125);
-INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:04:19:8E', false, -31.6403175, -51.4018125);
+INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:08:FC:DD', true, -34.6403175, -58.4018125);
+INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:04:19:2F', true, -33.6403175, -59.4018125);
+INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:04:18:A0', true, -32.6403175, -50.4018125);
+INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:04:19:8E', true, -31.6403175, -51.4018125);
 INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:04:1B:E4', false, -30.6403175, -52.4018125);
 INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:04:19:21', false, -35.6403175, -53.4018125);
 INSERT INTO Beacon(id, used, longitude, latitude) values ('0C:F3:EE:04:19:3E', false, -36.6403175, -54.4018125);
