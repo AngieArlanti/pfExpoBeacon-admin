0150892ea0ed81b4b6f29e1d4d29a1e5df26e314
AngieArlanti <angeles.arlanti@mercadolibre.com>
2020-03-09 23:20:09 -0300

    add stand form

--- frontend-pf-expo-beacon-admin/src/component/AddStandComponent.jsx
+++ frontend-pf-expo-beacon-admin/src/component/AddStandComponent.jsx
@@ -1,4 +1,20 @@
 import React, { Component } from "react";
+// reactstrap components
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Input,
+    Container,
+    Label,
+    Row,
+    Col,
+    InputGroup,
+    InputGroupAddon
+  } from "reactstrap";
 
 class AddStandComponent extends Component {
 
@@ -10,10 +26,91 @@ class AddStandComponent extends Component {
 
         return (
             <div className="container">
-                <h2>Add Stand</h2>
-        
+                <Card className="bg-secondary shadow">
+                    <CardHeader className="bg-white border-0">
+                        <Row className="align-items-center">
+                            <Col xs="8">
+                                <h3 className="mb-0">Add Stand</h3>
+                            </Col>
+                        </Row>
+                    </CardHeader>
+                    <CardBody>
+                        <Form>
+                            <FormGroup>
+                                <label className="form-control-label" htmlFor="input-title"> 
+                                    Title
+                                </label>
+                                <Input
+                                className="form-control-alternative"
+                                id="input-title"
+                                placeholder="Title"
+                                type="text"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <label className="form-control-label" htmlFor="input-beacon"> 
+                                    Beacon
+                                </label>
+                                <Input
+                                className="form-control-alternative"
+                                id="input-beacon"
+                                placeholder="Beacon"
+                                type="select"
+                                >
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <label className="form-control-label" htmlFor="input-short-description"> 
+                                    Short description
+                                </label>
+                                <Input
+                                className="form-control-alternative"
+                                id="input-short-description"
+                                placeholder="Short description"
+                                type="text"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <label className="form-control-label" htmlFor="input-description"> 
+                                    Description
+                                </label>
+                                <Input
+                                className="form-control-alternative"
+                                id="input-description"
+                                placeholder="Description"
+                                type="textarea"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <label className="form-control-label" htmlFor="input-cover"> 
+                                    Cover
+                                </label>
+                                <Input
+                                className="form-control-alternative"
+                                id="input-cover"
+                                placeholder="Cover"
+                                type="url"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <label className="form-control-label" htmlFor="input-pictures"> 
+                                    Pictures
+                                </label>
+                                <InputGroup>
+                                    <InputGroupAddon addonType="append"><Button>Add picture</Button></InputGroupAddon>
+                                    <Input />
+                                </InputGroup>
+                            </FormGroup>
+                        </Form>
+                    </CardBody>
+                </Card>
             </div>
-      )
+        )
     }
 } 
 
